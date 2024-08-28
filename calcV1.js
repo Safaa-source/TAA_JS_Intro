090c9c170d769334c48b72601645b0215b05c523
Safaa-source <safay410@gmail.com>
2024-08-28 23:11:36 +0200

    feat:understanding function

---
+++ calcV1.js
@@ -0,0 +1,19 @@
+// addition function 
+const add = (a , b) => a + b;
+// subtration function 
+const subract = (a , b) => a - b;
+// multiplication function 
+const multiply = (a , b) => a * b;
+// division function 
+const divide = (a , b) => a / b;
+
+let sum= add(6 , 3)
+let difference= subract(6 , 3)
+let product= multiply(6 , 3)
+let quotient= divide(6 , 3)
+
+
+console.log(`The sum is ${sum}`);
+console.log(`The difference is ${difference}`);
+console.log(`The product is ${product}`);
+console.log(`The quotient is ${quotient}`);
---
+++ functions.js
@@ -0,0 +1,39 @@
+// // function declaration
+// function sayHello(){
+//     console.log("Hello World");
+// }
+// // funtion calling 
+// sayHello();
+
+// function greet(name){
+//     console.log("hello " + name);
+// }
+// greet("safaa");
+
+
+// let a = 4
+// let b = 6
+// sum = a + b
+// function calculate(sum){
+//     console.log('sum:' + sum);
+// }
+
+// calculate(sum);
+
+function subtract(a , b){
+    return a-b;
+}
+let difference = subtract(9 , 3);
+console.log(`the difference is ${difference}`)
+
+function add(a , b){
+    return a+b;
+}
+let sum=add(6 ,2);
+console.log(`the sum is ${sum}`)
+
+function multiply(a , b){
+    return a * b;
+}
+let product=multiply(difference , sum);
+console.log(`${difference} x ${sum} = ${product}`)
