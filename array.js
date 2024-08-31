4fe16cc9ec6e4ef226247a1ecc0e7d959f13c0db
Safaa-source <safay410@gmail.com>
2024-08-31 09:16:54 +0200

    chore: learnt for loops

---
+++ array.js
@@ -0,0 +1,5 @@
+let fruits = ["apple" , "orange" , "watermelon" , "pineapple" , "strawberry"];  
+let numbers = [1,2,3,4,5,6,7,8,9];
+
+console.log(fruits[4]);
+console.log(numbers[3]);
---
+++ inventory.js
@@ -0,0 +1,10 @@
+let fruits = ["apple" , "orange" , "watermelon" , "pineapple" , "strawberry"];
+let vegetables = ["onions" , "peppers" , "cucumber" , "carrots" , "lettuce"];  
+let fastFood = ["burger" , "pizza" , "fries" , "tacos" , "sandwiches"];  
+
+console.log("== Inventory Management System ==")
+console.log("Fruits")
+for(let i = 0; i < fruits.length ; i++){
+    let j = i+1
+    console.log( j + ' ' +  fruits [i]);
+}
