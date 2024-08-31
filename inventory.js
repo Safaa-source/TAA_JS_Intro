cff3ff5f52bd8849cb25c53d1acb2d889f95179b
Safaa-source <safay410@gmail.com>
2024-08-31 09:32:57 +0200

    chore: learn while loops

--- inventory.js
+++ inventory.js
@@ -4,7 +4,16 @@ let fastFood = ["burger" , "pizza" , "fries" , "tacos" , "sandwiches"];
 
 console.log("== Inventory Management System ==")
 console.log("Fruits")
+// for loops
 for(let i = 0; i < fruits.length ; i++){
     let j = i+1
     console.log( j + ' ' +  fruits [i]);
 }
+console.log("Vegetables")
+// while loops
+let n = 0;
+while(n<vegetables.length){
+    let m = n+1; 
+    console.log( m + ' ' +  vegetables [n]);
+    n++;
+} 
